f54271b9e63e8f0f71902c593acfbcc4d48f2b47
Enzo Ahoué <ez@Enzos-MacBook-Pro.local>
2025-08-11 01:35:59 +0200

    Fix static render

--- jim/settings.py
+++ jim/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "django.contrib.staticfiles",
     'pages',
 ]
 
@@ -133,10 +132,13 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # Fichiers compressés + hashés pour la prod
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-# Base de données PostgreSQL depuis Render
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=False,  # Render mettra ssl_require=True si tu as une vraie DB
+    )
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
